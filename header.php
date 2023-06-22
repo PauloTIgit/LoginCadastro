2be09eae797437ac159993bab8f45afead9ae562
Paulo Fernando Ferreira Pires <92067743+PauloTIgit@users.noreply.github.com>
2023-06-22 13:41:00 -0300

    atualização do projeto

--- header.php
+++ header.php
@@ -8,12 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Site de aulas online">
     <link rel="stylesheet" type="text/css" href="./public/css/app.css">
-    <link rel="stylesheet" type="text/css" href="./public/css/main.css">
     <link rel="stylesheet" type="text/css" href="./public/css/root.css">
-    <link rel="stylesheet" type="text/css" href="./public/css/staff.css">
-    <link rel="stylesheet" type="text/css" href="./public/css/componente.css">
-    <link rel="stylesheet" type="text/css" href="./menu/menu.css">
-    <link rel="stylesheet" type="text/css" href="./public/css/404.css">
     <script src="https://kit.fontawesome.com/0b3e36b549.js" crossorigin="anonymous"></script>
 </head>
 
--- public/page/hero_page.php
+++ public/page/hero_page.php
@@ -1,19 +1,22 @@
 <header>
     <link rel="stylesheet" href="./public/css/hero.css">
 </header>
-<section class="main">
+<section class="main hero_page">
     <div class="header">
-        <div class="menu_hero">
-            <div class="list_hero">
-                <nav>
-                    <a href="">Seviços</a>
-                    <a href="">Shop</a>
-                    <a href="">Contato</a>
-                </nav>
-                <nav>
-                    <a href="">Acessar</a>
+        <div class="menu">
+            <div class="menu_box">
+                <div class="menu_header logo">
+                    <img  src="" alt="">
+                </div>
+                <nav class="menu_lista">
+                    <a href="" class="link"></a>
+                    <a href="" class="link">Blog</a>
+                    <a href="" class="link">Acessar</a>
                 </nav>
             </div>
         </div>
+    </div>
+    <div class="corpo">
+
     </div>
 </section>
